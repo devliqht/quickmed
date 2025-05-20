a4dd6b0e02ba0e9bd82ad8965eb23873abe3f770
Matt Cabarrubias <monapwc2020@gmail.com>
2025-05-20 18:02:31 +0800

    feat: add util functions

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,42 @@
-import { type ClassValue, clsx } from 'clsx';
+import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Combines class names with Tailwind CSS classes
+ */
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+/**
+ * Format date to a readable string
+ */
+export function formatDate(date: Date | string): string {
+    return new Date(date).toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    });
+}
+
+/**
+ * Format time to a readable string
+ */
+export function formatTime(date: Date | string): string {
+    return new Date(date).toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+}
+
+/**
+ * Generate initials from a name
+ */
+export function getInitials(name: string): string {
+    return name
+        .split(' ')
+        .map(part => part[0])
+        .join('')
+        .toUpperCase()
+        .substring(0, 2);
 }
